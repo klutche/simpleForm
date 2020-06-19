a691255edc6984fef18e3c7b4f8e7c762fa5c20c
KLUTCHE <klutche@gmail.com>
2020-06-19 20:21:41 +0900

    全体的に修正

--- index.php
+++ index.php
@@ -4,31 +4,37 @@ $send_flg = false;
 // エラーメッセージ
 $err_msg = array();
 // 送信ボタンを押した後の処理
-if ($_POST["post_flg"]) {
+if ($_POST['post_flg']) {
 	// エラー
-	if ($_POST["name"] == "") {
-		$err_msg[] = "「お名前」は必須です";
+	if ($_POST['name'] == '') {
+		$err_msg['name'] = '「お名前」は必須です';
 	}
-	if ($_POST["email"] == "") {
-		$err_msg[] = "「メールアドレス」は必須です";
+	if ($_POST['email'] == '') {
+		$err_msg['email'] = '「メールアドレス」は必須です';
+	}
+	if ($_POST['comment'] == '') {
+		$err_msg['comment'] = '「お問い合わせ内容」を入力してください';
 	}
 	// エラーが無ければ送信
-	if (count($err_msg) == 0) {
+	if (count( $err_msg) == 0) {
 		// 宛先
-		$mail_to = "kurahara@downtownkumamoto.com";
+		$mail_to = 'shekuhara@gmail.com';
 		// 送信元アドレス
-		$mail_from = "form@example.com";
+		$mail_from = 'form@example.com';
 		// 件名
-		$mail_subject = "メールフォームから送信がありました";
+		$mail_subject = 'メールフォームから送信がありました';
 		// 本文
-		$mail_body = "";
-		$mail_body.= "■お名前\n".$_POST["name"]."\n\n";
-		$mail_body.= "■メールアドレス\n".$_POST["email"]."\n\n";
-		$mail_body.= "■コメント\n".$_POST["comment"];
+		$mail_body = '';
+		$mail_body .= '■お名前'."\n".$_POST['name']."\n\n";
+		$mail_body .= '■メールアドレス'."\n".$_POST['email']."\n\n";
+		if ($_POST['tel'] != '') {
+			$mail_body .= '■お電話番号'."\n".$_POST['tel']."\n\n";
+		}
+		$mail_body .= '■お問い合わせ内容'."\n".$_POST['comment'];
 		// 送信処理
-		mb_language("Japanese");
-		mb_internal_encoding("UTF-8");
-		mb_send_mail($mail_to, $mail_subject, $mail_body, "From: <".$mail_from.">");
+		mb_language('Japanese');
+		mb_internal_encoding('UTF-8');
+		mb_send_mail($mail_to, $mail_subject, $mail_body, 'From: <' . $mail_from . '>');
 		$send_flg = true;
 		$_POST = array();
 	}
@@ -37,55 +43,34 @@ if ($_POST["post_flg"]) {
 <!DOCTYPE HTML>
 <html lang="ja">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta charset="UTF-8">
 <title>メールフォーム</title>
 </head>
-
 <body>
+<h1>メールフォーム</h1>
 <?php
 // 初期フォーム表示
 if (!$send_flg) {
 ?>
-
-<h1>メールフォーム</h1>
-
-<?php
-// エラーメッセージがある場合
-if (count($err_msg) > 0) {
-?>
-<p style="color:red;">
-<?php foreach ($err_msg as $val) { ?>
-※<?php echo $val ?><br>
-<?php } ?>
-</p>
-<?php } ?>
-
 <form action="" method="post">
-
-<p>
-お名前<br>
-<input type="text" name="name" value="<?php echo $_POST["name"] ?>"><br>
-</p>
-
-<p>
-メールアドレス<br>
-<input type="text" name="email" value="<?php echo $_POST["email"] ?>"><br>
-</p>
-
-<p>
-コメント<br>
-<textarea name="comment" rows="10"><?php echo $_POST["comment"] ?></textarea><br>
-</p>
-
+<p>お名前</p>
+<input type="text" name="name" value="<?= @$_POST['name']; ?>"><br>
+<?= $err_msg['name'] ? '<p style="color: #f00;">'.$err_msg['name'].'</p>' : ''; ?>
+<p>メールアドレス</p>
+<input type="email" name="email" value="<?= @$_POST['email']; ?>"><br>
+<?= $err_msg['email'] ? '<p style="color: #f00;">'.$err_msg['email'].'</p>' : ''; ?>
+<p>お電話番号</p>
+<input type="text" name="tel" value="<?= @$_POST['tel']; ?>"><br>
+<p>お問い合わせ内容</p>
+<textarea name="comment" rows="10"><?= @$_POST['comment']; ?></textarea><br>
 <input type="submit" name="post_flg" value="送信する"><br>
 </form>
-
 <?php
 // メール送信後の表示
 } else {
 ?>
-メールを送信しました。<br>
+<p>メールを送信いたしました。<br>
+お問い合わせありがとうございます。</p>
 <?php } ?>
-
 </body>
 </html>
